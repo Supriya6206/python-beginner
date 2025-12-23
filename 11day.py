3051078755331ecdf969022e7bff160b3ff4502f
[supriya.stha.0601@gmail.com] <supriya.stha.0601@gmail.com>
2025-12-23 23:04:52 +0545

    Refactor marks evaluation logic and update Nepali marks

--- 11day.py
+++ 11day.py
@@ -8,14 +8,26 @@ print("Name:",student["name"])
 print("Age:",student["age"])
 print("Course:",student["course"])
 
+# marks = {
+#     "Math":75,
+#     "Physics":85,
+#     "Nepali":90
+# }
+
+# if marks["Physics"] >=40:
+#     print("You have passed in Physics")
+    
+# else:
+#     print("You have failed in Physics")
+
 marks = {
     "Math":75,
     "Physics":85,
-    "Nepali":90
+    "Nepali":30
 }
 
-if marks["Physics"] >=40:
-    print("You have passed in Physics")
+if marks["Physics"] >=40 and marks["Math"] >=40 and marks["Nepali"] >=40  :
+    print("You have passed")
     
 else:
-    print("You have failed in Physics")
+    print("You have failed")
